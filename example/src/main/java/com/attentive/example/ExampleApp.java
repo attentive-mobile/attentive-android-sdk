4c150a75a1251da073d61cf7b9ba0298cef56ca1
Olivia <oliviakim321@gmail.com>
2022-09-07 09:58:51 -0700

    add more comments

--- example/src/main/java/com/attentive/example/ExampleApp.java
+++ example/src/main/java/com/attentive/example/ExampleApp.java
@@ -4,13 +4,16 @@ import android.app.Application;
 import com.attentive.mobilesdk.AttentiveConfig;
 
 public class ExampleApp extends Application {
-    private static final String ATTENTIVE_DOMAIN = "games";
+    // Change this to your Attentive Domain to test with your Attentive account
+    private static final String ATTENTIVE_DOMAIN = "YOUR_ATTENTIVE_DOMAIN";
+    // The mode in which to run the Attentive Mobile SDK
+    private static final AttentiveConfig.Mode MODE = AttentiveConfig.Mode.PRODUCTION;
+
     public AttentiveConfig attentiveConfig;
 
     @Override
     public void onCreate() {
-        attentiveConfig = new AttentiveConfig(
-                ATTENTIVE_DOMAIN, AttentiveConfig.Mode.PRODUCTION);
+        this.attentiveConfig = new AttentiveConfig(ATTENTIVE_DOMAIN, MODE);
         super.onCreate();
     }
 }
