105dc18cb1d0eee8acadbfb4ed2f81289aeb5256
Wyatt Davis <wdavis@attentivemobile.com>
2022-11-03 16:50:34 -0700

    Adds Android test directories

---
+++ attentive-android-sdk/src/test/java/com/attentive/androidsdk/AttentiveConfigTest.java
@@ -0,0 +1,21 @@
+package com.attentive.androidsdk;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AttentiveConfigTest {
+    @Test
+    public void constructor_validParams_gettersReturnConstructorParams() {
+        // Arrange
+        final String domain = "domain";
+        final AttentiveConfig.Mode mode = AttentiveConfig.Mode.DEBUG;
+
+        // Act
+        AttentiveConfig config = new AttentiveConfig(domain, mode);
+
+        // Assert
+        assertEquals(domain, config.getDomain());
+        assertEquals(mode, config.getMode());
+    }
+}
