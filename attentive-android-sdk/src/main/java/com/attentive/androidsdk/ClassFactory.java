e8d66d4910bdd3ce769c67dc971f8df7a46146cf
Wyatt Davis <wdavis@attentivemobile.com>
2022-11-22 11:29:31 -0800

    add jackson

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/ClassFactory.java
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/ClassFactory.java
@@ -1,6 +1,7 @@
 package com.attentive.androidsdk;
 
 import android.content.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClassFactory {
     public static PersistentStorage buildPersistentStorage(Context context) {
@@ -9,5 +10,9 @@ public class ClassFactory {
 
     public static VisitorService buildVisitorService(PersistentStorage persistentStorage) {
         return new VisitorService(persistentStorage);
+    }
+
+    public static ObjectMapper buildObjectMapper() {
+        return new ObjectMapper();
     }
 }
