9342067faac1470220a8c03d30436654777cc469
Wyatt Davis <wyattjoe40@live.com>
2022-11-23 11:32:03 -0800

    Send user identifiers to events endpoint (#15)

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/ClassFactory.java
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/ClassFactory.java
@@ -1,6 +1,8 @@
 package com.attentive.androidsdk;
 
 import android.content.Context;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
 
 public class ClassFactory {
     public static PersistentStorage buildPersistentStorage(Context context) {
@@ -9,5 +11,17 @@ public class ClassFactory {
 
     public static VisitorService buildVisitorService(PersistentStorage persistentStorage) {
         return new VisitorService(persistentStorage);
+    }
+
+    public static ObjectMapper buildObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    public static OkHttpClient buildOkHttpClient() {
+        return new OkHttpClient.Builder().build();
+    }
+
+    public static AttentiveApi buildAttentiveApi(OkHttpClient okHttpClient, ObjectMapper objectMapper) {
+        return new AttentiveApi(okHttpClient, objectMapper);
     }
 }
