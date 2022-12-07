46864273c3d163e5519c3529c6ac820f63c2e4e6
Wyatt Davis <wdavis@attentivemobile.com>
2022-12-06 18:03:42 -0800

    wip

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/AttentiveEventTracker.java
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/AttentiveEventTracker.java
@@ -1,4 +1,30 @@
 package com.attentive.androidsdk;
 
 public class AttentiveEventTracker {
+    private static AttentiveEventTracker INSTANCE;
+
+    public static void initialize(AttentiveConfig config) {
+        if (INSTANCE != null) {
+            throw new IllegalStateException("Initialize already called");
+        }
+
+        INSTANCE = new AttentiveEventTracker(config);
+    }
+
+    public static AttentiveEventTracker getInstance() {
+        if (INSTANCE == null) {
+            throw new IllegalStateException("Initialize must be called");
+        }
+
+        return INSTANCE;
+    }
+
+    private final AttentiveConfig config;
+
+    private AttentiveEventTracker(AttentiveConfig config) {
+        this.config = config;
+    }
+
+    public void recordEvent(Event event) {
+    }
 }
