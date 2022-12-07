9f1d0f9345d79a51141516dc361c636adf13f496
Wyatt Davis <wdavis@attentivemobile.com>
2022-12-06 17:12:03 -0800

    more

--- example/src/main/java/com/attentive/example/activities/ProductPageActivity.java
+++ example/src/main/java/com/attentive/example/activities/ProductPageActivity.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.attentive.androidsdk.AttentiveAnalytics;
 import com.attentive.androidsdk.Product;
 import com.attentive.androidsdk.ProductView;
-import com.attentive.androidsdk.Purchase;
 import com.attentive.example.R;
-import java.util.List;
 
 public class ProductPageActivity extends AppCompatActivity {
 
@@ -19,7 +16,7 @@ public class ProductPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_page);
     }
 
-    public void sendPurchaseEvent(View view) {
+    public void purchaseButtonClicked(View view) {
         // TODO
     }
 }
