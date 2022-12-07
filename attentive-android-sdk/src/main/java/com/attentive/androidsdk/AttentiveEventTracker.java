acb20539f22e9b35f49514894b0a16b12608ef94
Wyatt Davis <wdavis@attentivemobile.com>
2022-12-06 17:57:57 -0800

    more

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/AttentiveEventTracker.java
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/AttentiveEventTracker.java
@@ -1,11 +1,4 @@
 package com.attentive.androidsdk;
 
 public class AttentiveEventTracker {
-    private final AttentiveConfig config;
-
-    public AttentiveEventTracker(AttentiveConfig config) {
-        ParameterValidation.verifyNotNull(config, "config");
-
-        this.config = config;
-    }
 }
