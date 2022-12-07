c04c4d127a322d3cbd989530e4a7c6a824f3f91b
Wyatt Davis <wyattjoe40@live.com>
2022-12-07 10:01:58 -0800

    Adds purchase UI to the Example app (#21)
    * Adds purchase activity to the ui
    
    * more

---
+++ example/src/main/java/com/attentive/example/activities/ProductPageActivity.java
@@ -0,0 +1,20 @@
+package com.attentive.example.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.attentive.example.R;
+
+public class ProductPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_page);
+    }
+
+    public void purchaseButtonClicked(View view) {
+        // TODO
+    }
+}
