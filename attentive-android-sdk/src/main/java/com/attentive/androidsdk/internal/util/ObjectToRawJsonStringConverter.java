a873ceccc858bc277b7b344197f7ae7eb89ecf4c
Olivia <oliviakim321@gmail.com>
2023-01-19 14:42:03 -0500

    ObjectToRawJsonStringConverter tests

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/internal/util/ObjectToRawJsonStringConverter.java
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/internal/util/ObjectToRawJsonStringConverter.java
@@ -2,14 +2,9 @@ package com.attentive.androidsdk.internal.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.util.StdConverter;
 
-// Some of our DTOs need to serialize nested objects into strings
-//
-// For example:
-// Instead of this: {"key": ["one", "two"]}
-//    We want this: {"key": "[\"one\", \"two\"]"}
+
 public class ObjectToRawJsonStringConverter extends StdConverter<Object, String> {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
