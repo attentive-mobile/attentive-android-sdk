50d6b8e67876bf256b02fff4ac4c8f29e27f5505
Wyatt Davis <wdavis@attentivemobile.com>
2023-01-29 15:49:56 -0800

    Adds Kotlin example app

---
+++ example-kotlin/src/main/java/com/attentive/example_kotlin/ExampleKotlinApp.kt
@@ -0,0 +1,33 @@
+package com.attentive.example_kotlin
+
+import android.app.Application
+import com.attentive.androidsdk.AttentiveConfig
+import com.attentive.androidsdk.AttentiveEventTracker
+import com.attentive.androidsdk.UserIdentifiers
+
+class ExampleKotlinApp : Application() {
+    lateinit var attentiveConfig: AttentiveConfig
+
+    companion object {
+        fun buildUserIdentifiers(): UserIdentifiers {
+            // Add all the identifiers that you have for the current user
+            return UserIdentifiers.Builder()
+                .withPhone("+15556667777")
+                .withEmail("some_email@gmailfake.com")
+                .withKlaviyoId("userKlaviyoId")
+                .withShopifyId("userShopifyId")
+                .withClientUserId("userClientUserId")
+                .withCustomIdentifiers(mapOf(Pair("customIdentifierKey", "customIdentifierValue")))
+                .build()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        attentiveConfig = AttentiveConfig("games", AttentiveConfig.Mode.PRODUCTION, this)
+        AttentiveEventTracker.getInstance().initialize(attentiveConfig)
+
+        // Register the current user with the Attentive SDK. This should be done as early as possible.
+        attentiveConfig.identify(buildUserIdentifiers())
+    }
+}
--- example/src/main/java/com/attentive/example/ExampleApp.java
+++ example/src/main/java/com/attentive/example/ExampleApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import com.attentive.androidsdk.AttentiveConfig;
 import com.attentive.androidsdk.AttentiveEventTracker;
 import com.attentive.androidsdk.UserIdentifiers;
+import java.util.Map;
 
 public class ExampleApp extends Application {
     // Change this to your Attentive Domain to test with your Attentive account
@@ -16,20 +17,25 @@ public class ExampleApp extends Application {
 
     @Override
     public void onCreate() {
+        super.onCreate();
+
         this.attentiveConfig = new AttentiveConfig(ATTENTIVE_DOMAIN, MODE, getApplicationContext());
         // "initialize" must be called before any other methods on the AttentiveEventTracker instance
         AttentiveEventTracker.getInstance().initialize(attentiveConfig);
 
         // Register the current user with the Attentive SDK. This should be done as early as possible.
-        // Replace "APP_USER_ID" with the current user's ID.
         attentiveConfig.identify(buildUserIdentifiers());
-
-        super.onCreate();
     }
 
     public static UserIdentifiers buildUserIdentifiers() {
-            return new UserIdentifiers.Builder()
-                .withClientUserId("CLIENT_USER_ID")
-                .build();
+        // Add all the identifiers that you have for the current user
+        return new UserIdentifiers.Builder()
+            .withPhone("+15556667777")
+            .withEmail("some_email@gmailfake.com")
+            .withKlaviyoId("userKlaviyoId")
+            .withShopifyId("userShopifyId")
+            .withClientUserId("userClientUserId")
+            .withCustomIdentifiers(Map.of("customIdentifierKey", "customIdentifierValue"))
+            .build();
     }
 }
