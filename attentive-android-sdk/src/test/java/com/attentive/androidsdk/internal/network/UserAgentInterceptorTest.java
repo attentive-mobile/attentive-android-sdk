98373846ad0575d579dbbc8fd96d41972c9fd1aa
Wyatt Davis <wdavis@attentivemobile.com>
2023-03-10 07:45:14 -0800

    Adds test

--- attentive-android-sdk/src/test/java/com/attentive/androidsdk/internal/network/UserAgentInterceptorTest.java
+++ attentive-android-sdk/src/test/java/com/attentive/androidsdk/internal/network/UserAgentInterceptorTest.java
@@ -1,6 +1,7 @@
 package com.attentive.androidsdk.internal.network;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -18,8 +19,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 public class UserAgentInterceptorTest {
+    private static final String APP_NAME = "appNameValue";
+    private static final String APP_VERSION = "appVersionValue";
+    private static final String APP_PACKAGE_NAME = "com.what.exampleapp";
+    private static final String ANDROID_VERSION = "androidVersion";
+    private static final String ANDROID_LEVEL = "androidLevel";
+    private static final String ATTENTIVE_SDK_VERSION = "attentiveSdkValue";
+
     private UserAgentInterceptor userAgentInterceptor;
 
     @Before
@@ -43,6 +52,21 @@ public class UserAgentInterceptorTest {
         verify(chain).proceed(requestArgumentCaptor.capture());
         Request requestWithUserAgent = requestArgumentCaptor.getValue();
         assertEquals(testAgent, requestWithUserAgent.header("User-Agent"));
+    }
+
+    @Test
+    public void getUserAgent_returnsCorrectlyFormattedUserAgent() {
+        try (MockedStatic<AppInfo> appInfoMockedStatic = Mockito.mockStatic(AppInfo.class)) {
+            appInfoMockedStatic.when(() -> AppInfo.getApplicationName(any())).thenReturn(APP_NAME);
+            appInfoMockedStatic.when(() -> AppInfo.getApplicationVersion(any())).thenReturn(APP_VERSION);
+            appInfoMockedStatic.when(() -> AppInfo.getApplicationPackageName(any())).thenReturn(APP_PACKAGE_NAME);
+            appInfoMockedStatic.when(AppInfo::getAndroidLevel).thenReturn(ANDROID_LEVEL);
+            appInfoMockedStatic.when(AppInfo::getAndroidVersion).thenReturn(ANDROID_VERSION);
+            appInfoMockedStatic.when(AppInfo::getAttentiveSDKVersion).thenReturn(ATTENTIVE_SDK_VERSION);
+
+            String userAgent = userAgentInterceptor.getUserAgent();
+            assertEquals("attentive-android-sdk/" + ATTENTIVE_SDK_VERSION + " (Android " + ANDROID_VERSION + "; Android API Level " + ANDROID_LEVEL + ") " + APP_NAME + "/" + APP_VERSION + " (" + APP_PACKAGE_NAME + ")", userAgent);
+        }
     }
 
     private Request buildRequest() {
