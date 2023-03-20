38e011a82f22a7de7d47d292b65d648f550c68b4
Wyatt Davis <wdavis@attentivemobile.com>
2023-03-20 16:30:04 -0700

    WIP

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/AttentiveApi.java
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/AttentiveApi.java
@@ -6,12 +6,14 @@ import androidx.annotation.Nullable;
 
 import androidx.annotation.VisibleForTesting;
 import com.attentive.androidsdk.events.AddToCartEvent;
+import com.attentive.androidsdk.events.CustomEvent;
 import com.attentive.androidsdk.events.Event;
 import com.attentive.androidsdk.events.Item;
 import com.attentive.androidsdk.events.ProductViewEvent;
 import com.attentive.androidsdk.events.PurchaseEvent;
 import com.attentive.androidsdk.internal.events.InfoEvent;
 import com.attentive.androidsdk.internal.network.AddToCartMetadataDto;
+import com.attentive.androidsdk.internal.network.CustomEventMetadataDto;
 import com.attentive.androidsdk.internal.network.Metadata;
 import com.attentive.androidsdk.internal.network.OrderConfirmedMetadataDto;
 import com.attentive.androidsdk.internal.network.ProductDto;
@@ -271,7 +273,8 @@ class AttentiveApi {
             ORDER_CONFIRMED("oc"),
             PRODUCT_VIEW("d"),
             ADD_TO_CART("c"),
-            INFO("i");
+            INFO("i"),
+            CUSTOM_EVENT("ce");
 
             private final String abbreviation;
 
@@ -399,6 +402,14 @@ class AttentiveApi {
             }
         } else if (event instanceof InfoEvent) {
             eventRequests.add(new EventRequest(new Metadata(), EventRequest.Type.INFO));
+        } else if (event instanceof CustomEvent) {
+            CustomEvent customEvent = (CustomEvent) event;
+
+            CustomEventMetadataDto metadataDto = new CustomEventMetadataDto();
+            metadataDto.setType(customEvent.getType());
+            metadataDto.setProperties(customEvent.getProperties());
+
+            eventRequests.add(new EventRequest(metadataDto, EventRequest.Type.CUSTOM_EVENT));
         } else {
             final String error = "Unknown Event type: " + event.getClass().getName();
             Log.e(this.getClass().getName(), error);
