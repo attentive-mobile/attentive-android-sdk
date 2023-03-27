8fc8b6d04ff3446f2a773246ed31ab5958496d6c
Olivia <oliviakim321@gmail.com>
2023-03-27 15:03:40 -0400

    suppress warnings

--- attentive-android-sdk/src/test/java/android/util/Log.java
+++ attentive-android-sdk/src/test/java/android/util/Log.java
@@ -1,21 +1,25 @@
 package android.util;
 
 public class Log {
+    @SuppressWarnings("checkstyle:methodname")
     public static int d(String tag, String msg) {
         System.out.println("DEBUG: " + tag + ": " + msg);
         return 0;
     }
 
+    @SuppressWarnings("checkstyle:methodname")
     public static int i(String tag, String msg) {
         System.out.println("INFO: " + tag + ": " + msg);
         return 0;
     }
 
+    @SuppressWarnings("checkstyle:methodname")
     public static int w(String tag, String msg) {
         System.out.println("WARN: " + tag + ": " + msg);
         return 0;
     }
 
+    @SuppressWarnings("checkstyle:methodname")
     public static int e(String tag, String msg) {
         System.out.println("ERROR: " + tag + ": " + msg);
         return 0;
