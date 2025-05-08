c7e30684edcdab5c7a7fd4675f864beb25e0f993
Tyler Pfaff <tpfaff@attentivemobile.com>
2025-05-08 11:20:59 -0700

    Fix some tests

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/tracking/AppLaunchTracker.kt
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/tracking/AppLaunchTracker.kt
@@ -2,6 +2,7 @@ package com.attentive.androidsdk.tracking
 
 import android.app.Application
 import android.content.Context
+import android.os.Looper
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -10,7 +11,7 @@ import com.attentive.androidsdk.AttentiveConfig
 import com.attentive.androidsdk.AttentiveEventTracker
 import timber.log.Timber
 
-class AppLaunchTracker( ) : DefaultLifecycleObserver {
+class AppLaunchTracker(internal val lifecycle: Lifecycle = ProcessLifecycleOwner.get().lifecycle) : DefaultLifecycleObserver {
     override fun onStart(owner: LifecycleOwner) {
         super.onStart(owner)
         Timber.d("App moved to foreground")
@@ -20,7 +21,6 @@ class AppLaunchTracker( ) : DefaultLifecycleObserver {
     }
 
     fun registerAppLaunchTracker() {
-        val lifecycle = ProcessLifecycleOwner.get().lifecycle
         lifecycle.addObserver(this)
 
         if (lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
