01b5638ead8d860d6901bd6c47c6cb46fdc30bc7
Tyler Pfaff <tylerpfaff@gmail.com>
2025-08-25 18:02:13 -0700

    NATV-110 Improve log filtering options (#129)

--- attentive-android-sdk/src/main/java/com/attentive/androidsdk/internal/util/StandardTree.kt
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/internal/util/StandardTree.kt
@@ -7,12 +7,13 @@ import timber.log.Timber
 class StandardTree : Timber.Tree() {
     @SuppressLint("LogNotTimber")
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        val attentiveMessage = "Attentive: $message"
         if (priority == Log.ERROR) {
-            Log.e(tag, message, t)
+            Log.e(tag, attentiveMessage, t)
         } else if (priority == Log.WARN) {
-            Log.w(tag, message, t)
+            Log.w(tag, attentiveMessage, t)
         } else if (priority == Log.INFO) {
-            Log.i(tag, message, t)
+            Log.i(tag, attentiveMessage, t)
         }
     }
 }
