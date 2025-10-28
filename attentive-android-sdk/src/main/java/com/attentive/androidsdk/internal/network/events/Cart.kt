c2de10bb08e78941739d7bcec1f4f1d732e17a7e
Tyler Pfaff <tpfaff@attentivemobile.com>
2025-10-28 14:37:19 -0700

    Break up new model files

---
+++ attentive-android-sdk/src/main/java/com/attentive/androidsdk/internal/network/events/Cart.kt
@@ -0,0 +1,11 @@
+package com.attentive.androidsdk.internal.network.events
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Cart(
+    val cartTotal: String? = null,
+    val cartCoupon: String? = null,
+    val cartDiscount: String? = null,
+    val cartId: String? = null
+)
