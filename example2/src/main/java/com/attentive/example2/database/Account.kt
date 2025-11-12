4357e8f47b53e94f041e68e45d09b2eb61c61813
Tyler Pfaff <tylerpfaff@gmail.com>
2025-11-12 15:11:50 -0800

    NATV-155 Rename all instances of example2 to bonni (#147)

--- example2/src/main/java/com/attentive/example2/database/Account.kt
+++
@@ -1,7 +0,0 @@
-package com.attentive.example2.database
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "accounts")
-data class Account(@PrimaryKey val email: String, val password: String, val signedIn: Boolean = false)
